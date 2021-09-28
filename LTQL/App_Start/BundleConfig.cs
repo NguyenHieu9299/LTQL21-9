b9f6215e9e108e71443aaa117d6ff45cfef083b6
hieunguyen <hieunguyen@DESKTOP-O8TN4L4>
2021-09-28 14:28:50 +0700

    new

--- LTQL/App_Start/BundleConfig.cs
+++ LTQL/App_Start/BundleConfig.cs
@@ -28,6 +28,7 @@ namespace LTQL
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+                     
         }
     }
 }
