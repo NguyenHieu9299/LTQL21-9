24f9bca05792a6bf86aa6ad4bd41e1d64aa0f3f6
hieunguyen <hieunguyen@DESKTOP-O8TN4L4>
2021-09-28 13:39:32 +0700

    28-9

--- LTQL/App_Start/BundleConfig.cs
+++ LTQL/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace LTQL
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/ckEditor").Include(
+                       "~/Scripts/ckeditor/ckeditor.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
